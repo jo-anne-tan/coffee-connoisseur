473ce22f932b7d51137f6371293206feebe0cbbd
Jo-Anne Tan <tanjoanne128@gmail.com>
2022-06-03 11:50:10 +0800

    extend tw background image

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type Props = {
   handleOnClick: () => void;
   buttonText: string;
@@ -7,10 +5,7 @@ type Props = {
 
 const Banner: React.FC<Props> = ({ handleOnClick, buttonText }) => {
   return (
-    <div
-      className="relative block p-5 rounded-xl w-[80vw] h-96"
-      style={{ backgroundImage: 'url("/static/hero.png")' }}
-    >
+    <div className="relative block p-5 rounded-xl w-[80vw] h-96 bg-hero">
       <h1 className="mt-3 text-[60px] font-extrabold">
         Coffee <span className="text-pink-300">Connoisseur</span>
       </h1>
@@ -21,16 +16,6 @@ const Banner: React.FC<Props> = ({ handleOnClick, buttonText }) => {
       >
         {buttonText}
       </button>
-      {/* <div className="absolute -top-5 -left-5 -z-10">
-        <Image
-          alt="hero"
-          src="/static/hero.png"
-          objectFit="cover"
-          className=" rounded-xl opacity-80"
-          width={1200}
-          height={700}
-        />
-      </div> */}
     </div>
   );
 };
