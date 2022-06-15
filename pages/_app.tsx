5f879c796c79a35661438d38c783b53e4365a6e4
Jo-Anne Tan <tanjoanne128@gmail.com>
2022-06-15 15:48:24 +0800

    add React.StrictMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,30 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import StoreProvider from "../context/store-context";
 import { SWRConfig } from "swr";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
-    >
-      <div
-        className="p-10 min-h-screen text-gray-700"
-        style={{
-          backgroundImage: "url(/static/mesh-gradient.png)",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "50%",
-          height: "100%",
-        }}
+    <React.StrictMode>
+      <SWRConfig
+        value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
       >
-        <StoreProvider>
-          <Component {...pageProps} />
-        </StoreProvider>
-      </div>
-    </SWRConfig>
+        <div
+          className="p-10 min-h-screen text-gray-700"
+          style={{
+            backgroundImage: "url(/static/mesh-gradient.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "50%",
+            height: "100%",
+          }}
+        >
+          <StoreProvider>
+            <Component {...pageProps} />
+          </StoreProvider>
+        </div>
+      </SWRConfig>
+    </React.StrictMode>
   );
 }
 
